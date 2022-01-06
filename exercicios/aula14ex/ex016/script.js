26bbdbf6d21c63890a1a5d47d954a1d530734147
RodriWho <victorrodrigofreitas@gmail.com>
2022-01-06 17:28:14 -0300

    atualizando

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,13 +1,32 @@
 function contar() {
-    var inicio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
-    var res = window.document.getElementById('p')
-    var i = Number(inicio.value)
-    var f = Number(fim.value)
-    var p = Number(passo.value)
+    let inicio = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
 
-    for (c = i; c >= f; p) {
-        res.innerText = c
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+       //window.alert('[ERRO] Faltam dados!')
+       res.innerHTML = 'Impossivel contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `\u{1F449} ${c}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `\u{1F449} ${c}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
