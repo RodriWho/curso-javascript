f6e8ad95a4c4aeaf125be130ead7037ff7a00d53
RodriWho <victorrodrigofreitas@gmail.com>
2022-01-08 10:18:24 -0300

    atualizado

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -4,8 +4,8 @@ let soma = 0
 function adicionar() {
     let numero = document.getElementById('txtN')
     let tab = document.getElementById('txtS')
-    let baixo = document.getElementById('baixo')
-    n = Number(numero.value)
+    let pFin = document.getElementById('pFin')
+    n = Number.parseInt(numero.value)
 
     let pos = vetor.indexOf(n) // Buscar valor n no vetor
 
@@ -16,6 +16,7 @@ function adicionar() {
             alert(`O valor ${n} é Invalido`)
         } else {
             if (pos == -1) { // Verificar se valor ja registrado
+                pFin.innerHTML = ''
                 vetor.push(n)
                 let item = document.createElement('option')
                 item.text = `Valor ${n} Adicionado`
@@ -25,19 +26,23 @@ function adicionar() {
             }
         }
     }
-
 }
 
 function finalizar() {
-    vetor.sort()
-    somar()
-    baixo.innerHTML = `${vetor} <br>`
-    baixo.innerHTML += `Total Cadastrados: ${vetor.length} <br>`
-    baixo.innerHTML += `Maior valor cadastrado ${vetor[vetor.length -1]} <br>`
-    baixo.innerHTML += `Menor valor cadastrado ${vetor[0]} <br>`
-    baixo.innerHTML += `A soma dos valores é ${soma} <br>`
-    baixo.innerHTML += `A média total é ${soma / vetor.length}`
-
+    if (vetor == '') { // Verificar se foi adicionado valor ao vetor ao clicar em finalizar
+        alert('Adicione um valor na caixa!')
+    } else {
+        pFin.innerHTML = ''
+        vetor.sort()
+        somar()
+        pFin.innerHTML += `${vetor} <br>
+        Total Cadastrados: ${vetor.length} <br>
+        Maior valor cadastrado ${vetor[vetor.length -1]} <br>
+        Menor valor cadastrado ${vetor[0]} <br>
+        A soma dos valores é ${soma} <br>
+        A média total é ${soma / vetor.length}`
+    }
+    soma = 0
 }
 
 function somar() {
