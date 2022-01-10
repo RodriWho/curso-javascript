21f44787790a7b0343e6e9e7b6dd10c71ea0b154
RodriWho <96690420+RodriWho@users.noreply.github.com>
2022-01-10 15:39:24 -0300

    aula17 objetos criada

--- exercicios/aula17/objeto01.js
+++ exercicios/aula17/objeto01.js
@@ -1,2 +1,13 @@
-let amigo = {}
-console.log(typeof amigo)
+let amigo = {
+    nome: 'José',
+    sexo:'M',
+    peso: 85.4,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+
+console.log(amigo.nome, amigo.peso)
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg.`)
